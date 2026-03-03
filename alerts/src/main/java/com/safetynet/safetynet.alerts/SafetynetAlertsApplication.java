874a8e40b8a81e9507fe0a6ab1485e339c409210
Rebecca Guimaraes <rebekagc03@gamil.com>
2026-03-03 11:41:05 +0100

    chore: rename base package to com.safetynet.alerts

--- alerts/src/main/java/com/safetynet/safetynet.alerts/SafetynetAlertsApplication.java
+++ alerts/src/main/java/com/safetynet/safetynet.alerts/SafetynetAlertsApplication.java
@@ -1,4 +1,4 @@
-package com.safetynet.safetynet_alerts;
+package com.safetynet.alerts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
