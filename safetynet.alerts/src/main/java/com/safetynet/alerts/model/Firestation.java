bdcd38970dffbeecd0ddf2e1f297f2e2d91d7c36
Rebecca Guimaraes <rebekagc03@gamil.com>
2026-03-11 12:40:24 +0100

    add domain models

---
+++ safetynet.alerts/src/main/java/com/safetynet/alerts/model/Firestation.java
@@ -0,0 +1,25 @@
+package com.safetynet.alerts.model;
+
+public class Firestation {
+
+    private String address;
+    private String station;
+
+    public Firestation() {}
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+}
